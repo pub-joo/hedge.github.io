46cac66224dda143f55e36d6296daf24bbfba547
joo <andyjooyk@gmail.com>
2023-02-07 11:43:09 +0900

    test-1

---
+++ src/js/common.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+    $('.dialog-open').click(function(){
+        console.log('띠용');
+        $('.dialog').addClass('-active');
+    });
+    $('.dialog-close').click(function(){
+        console.log('닫힘');
+        $('.dialog').removeClass('-active');
+    });
+
+
+
+
+
+    $('.menu-open').click(function(){
+		$('.menu-wrap').stop().show().animate({"right":"0"},300);
+        $('.menu-dim').fadeIn(300);
+	}); 
+    $('.menu-close').click(function(){
+		$('.menu-wrap').stop().animate({"right":"-100%"},300).hide(300);
+        $('.menu-dim').fadeOut(300);
+	}); 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
